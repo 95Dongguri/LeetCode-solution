a3ea465d945769d4c8e2105e7809f747d2833f45
95Dongguri <hyuk_ey@naver.com>
2022-08-18 19:37:27 +0900

    Tree 문제 복습필요git add .

---
+++ 110 Balanced Binary Tree.playground/Contents.swift
@@ -0,0 +1,71 @@
+/*
+ Given a binary tree, determine if it is height-balanced.
+ 
+ For this problem, a height-balanced binary tree is defined as:
+ 
+ a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+ 
+ 
+ 
+ Example 1:
+ 
+ 
+ Input: root = [3,9,20,null,null,15,7]
+ Output: true
+ Example 2:
+ 
+ 
+ Input: root = [1,2,2,3,3,null,null,4,4]
+ Output: false
+ Example 3:
+ 
+ Input: root = []
+ Output: true
+ 
+ 
+ Constraints:
+ 
+ The number of nodes in the tree is in the range [0, 5000].
+ -104 <= Node.val <= 104
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        if root == nil { return true }
+        
+        return abs(checkDepth(root?.left) - checkDepth(root?.right)) <= 1 && isBalanced(root?.left) && isBalanced(root?.right)
+    }
+    
+    func checkDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        
+        let left = checkDepth(root?.left)
+        let right = checkDepth(root?.right)
+        
+        return 1 + max(left, right)
+    }
+}
